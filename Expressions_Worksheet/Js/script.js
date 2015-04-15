0202e7fa478b56ff1e15b33eb9b347b2727e4646
mrsdivastylz <mrs_gmanning@hotmail.com>
2015-04-15 18:48:09 -0400

    This is my Expressions Worksheet
    I wrote code to solve the Dog Years Problem by converting from Sparky
    human years to dog years

---
+++ Expressions_Worksheet/Js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Geneva Manning
+ * 04/15/2015
+ * Assignment: Expression Worksheet
+ * /
+
+ //Problem Dog Years
+ //Conversion which is done from Sparky humans years to dog years
+
+ // First I need to declare the variables
+
+ var sparkyAge;
+ var dogYears;
+ var result;
+
+ //Now i will define the variables
+
+ sparkyAge = prompt("How old is Sparky in human years?"); //This prompt will ask the user how old Sparky is in human years.
+ dogYears = sparkyAge * 7; //This is how the expression will be calculated.
+ result =("Sparky is " + sparkyAge + "human years old which is " + dogYears + " in dog years. " //This si the results from the calculation from calculating over from human years to dog years.
+
+ alert(result); //This is the calculation of what the user will get on their screen.
+ console.log(result); //This shows what is logged into the console.
+ // I said that sparky is was 3 human years old and and after the calculation the calculation was 21 in in dog years.
